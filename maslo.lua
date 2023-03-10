d8300a4a95060b3931996d86c6486767ebfe9d0c
AlreadyMAKS <97000211+AlreadyMAKS@users.noreply.github.com>
2023-03-10 17:34:36 +0200

    Update maslo.lua

--- maslo.lua
+++ maslo.lua
@@ -1,12 +1,13 @@
 local part = game.Workspace.Works.Courier.Model["Work_Courier"]
 part.Courier.HoldDuration = 0.001
 game.Players.LocalPlayer.Character:MoveTo(part.Position)
+game.Players.LocalPlayer.Character
 game.ReplicatedStorage.CourierWork.OnClientEvent:Connect(function(p1, p2)
-  game.Players.LocalPlayer.Character:MoveTo(p2.Position)
-   wait(0.2)
+  game.Players.LocalPlayer.Character.HumanoidRootPart.Position = p2.Position
+  wait(0.2)
   game.ReplicatedStorage.CourierWork:FireServer("Point")
-   wait(0.2)
+  wait(0.2)
   local part = game.Workspace.Works.Courier.Model["Work_Courier"]
   part.Courier.HoldDuration = 0.001
-  game.Players.LocalPlayer.Character:MoveTo(part.Position)
+  game.Players.LocalPlayer.Character.HumanoidRootPart.Position = part.Position
 end)
